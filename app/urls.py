67f88df2ed97624c9dfc123813e59a37b1cc201d
Michaelis <m.gregoriades@gmail.com>
2017-03-25 10:49:43 +0100

    MGKM

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.AppIndex, name="app_index"),
+    url(r'^add/$', views.AppAdd, name="app_add"),
+    url(r'^edit/(?P<app_id>[0-9]+)$', views.AppEdit, name="app_edit"),
+    url(r'^del/(?P<app_id>[0-9]+)$', views.AppDel, name="app_del"),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def AppIndex(request):
     return HttpResponse("This is juste a test")
+
+def AppAdd(request, app_id):
+    return HttpResponse("Add an App")
+
+def AppEdit(request, app_id):
+    return HttpResponse("Edit the App: " + app_id)
+
+def AppDel(request, app_id):
+    return HttpResponse("Del an App: " + app_id)
